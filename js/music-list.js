c8457b346d41ee41e16d7647aa4f8378ea52248f
zeddkhan03 <zeddkhan03@gmail.com>
2023-11-21 18:09:20 +0530

    Songs added

---
+++ js/music-list.js
@@ -0,0 +1,51 @@
+
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Save Your Tears - Weeknd",
+    artist: "Weeknd",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Dil Nu",
+    artist: "AP Dhillon",
+    img: "music-8",
+    src: "music-8"
+  },
+];
